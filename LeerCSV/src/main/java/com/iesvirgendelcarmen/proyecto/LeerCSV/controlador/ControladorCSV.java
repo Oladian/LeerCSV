f2f3985f6d66f3a5afc10ce0b95e7bbf2f06936a
Izan <ios2asir@gmail.com>
2018-06-14 12:45:27 +0200

    Hay poner en el POM que incluya dependencias

--- LeerCSV/src/main/java/com/iesvirgendelcarmen/proyecto/LeerCSV/controlador/ControladorCSV.java
+++ LeerCSV/src/main/java/com/iesvirgendelcarmen/proyecto/LeerCSV/controlador/ControladorCSV.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
